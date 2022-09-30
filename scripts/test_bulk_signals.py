0a891d17feb8c8ce96bc63a81dfd223c76495c19
cgarciaa1 <ca.garciaa1@uniandes.edu.co>
2022-09-29 20:04:46 -0500

    Mejoras scripts

--- scripts/test_bulk_signals.py
+++ scripts/test_bulk_signals.py
@@ -1,13 +1,21 @@
 # importing the requests library
 import requests
+import hashlib
 
 # api-endpoint
 URL = "http://192.168.1.145:5000/api-commands/signals"
 # defining a params dict for the parameters to be sent to the API
 PARAMS = {'evento' : 'Prueba'}
 
+salt = '12345678910'
+
 # sending get request and saving the response as response object 50 times
 for a in range(50):
+    value = '20° C'
+    hash = hashlib.sha512(value + salt).encode("utf-8")).hexdigest()
+    if a % 10 == 0
+        value = '30° C'
+    PARAMS = {'evento' : 'Temperatura', 'datos' : value, 'hash' : hash }
     r = requests.post(url = URL, json = PARAMS)
     print(r)
 
